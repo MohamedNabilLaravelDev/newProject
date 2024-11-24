a5066bad11a5cad33b863cdbce558a49d44c240d
MohamedNabil92 <121800442+MohamedNabil92@users.noreply.github.com>
2024-11-24 13:58:26 +0200

    ok all is good

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
 console.log("Hello World");
 console.log("Hello World 2");
 
+// i am in middle of feature
+// code not want to lose
+//Code
+//Code
+//Code
+//Code
+//Code
+
 console.log("Hello World 3");
